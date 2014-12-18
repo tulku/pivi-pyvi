e7a2186bf5e5e7a3fa807b792ca73b04c7489efb
Lucas Chiesa <lucas.chiesa@gmail.com>
2014-12-18 20:14:02 -0300

    Add unittest for xmega command parser

--- pyvi/tests/UnitTests.py
+++ pyvi/tests/UnitTests.py
@@ -2,6 +2,8 @@ from pyvi import ServerComm
 from pyvi import MCUComm
 from pyvi import Measurement
 
+from pyvi import TestTransport
+
 import unittest
 
 
@@ -33,6 +35,33 @@ class ServerCommTest(unittest.TestCase):
         t = self.s.unpack(pkg)
         self.assertEqual(self.m.id_, t.id_)
         self.assertEqual(self.m.Irms, t.Irms)
+
+
+class TransportTest(unittest.TestCase):
+
+    def setUp(self):
+        self.t = TestTransport()
+
+    def test_read_byte(self):
+        self.t._ans('j')
+        self.assertEqual('j', self.t.read())
+
+    def test_read_full_mcu_pkg(self):
+        self.t._ans("\x7E\xAA\x55\x00\x00\x7F")
+        pkg = self.t.read_package_from_xmega()
+        self.assertEqual((0xAA, 0x55), tuple(pkg))
+
+    def test_timeout_mcu_pkg(self):
+        # the package is missing the end pkg character.
+        self.t._ans("\x7E\xAA\x55\x00\x00")
+        pkg = self.t.read_package_from_xmega()
+        self.assertIsNone(pkg)
+
+    def test_stuffed_full_mcu_pkg(self):
+        self.t._ans("\x7E\x7D\x8A\x45\x00\x00\x7F")
+        pkg = self.t.read_package_from_xmega()
+        self.assertEqual((0xAA, 0x45), tuple(pkg))
+
 
 if __name__ == '__main__':
     unittest.main()
