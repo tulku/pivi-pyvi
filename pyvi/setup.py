51dd687b879577014a870386e1638f2b7d312c35
Lucas Chiesa <lucas.chiesa@gmail.com>
2015-02-25 01:25:10 -0300

    Correctly install Pyvi script

--- pyvi/setup.py
+++ pyvi/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='pyvi',
       version='0.1',
       packages=['pyvi', 'utils'],
-      scripts=['scripts/pyvi'],
+      scripts=['scripts/Pyvi'],
       # metadata for upload to PyPI
       author="Lucas Chiesa",
       author_email="lucas@lessindustries.com",
