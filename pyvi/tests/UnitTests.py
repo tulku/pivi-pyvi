2ac1158fc880bc6f41e0a13c09633ba3b19afda3
Lucas Chiesa <lucas.chiesa@gmail.com>
2015-03-08 19:35:29 -0300

    Fix unittests

--- pyvi/tests/UnitTests.py
+++ pyvi/tests/UnitTests.py
@@ -32,16 +32,16 @@ class MCUCommTest(unittest.TestCase):
 class ServerCommTest(unittest.TestCase):
 
     def setUp(self):
-        self.s = ServerComm()
+        self.s = ServerComm(pivi_id=0)
         self.m = Measurement()
-
+        self.mac = self.s.less_mac
         self.m.set(1, 15, 220, 0)
 
     def test_set(self):
         pkg = self.s.pack(self.m)
         t = self.s.unpack(pkg)
-        self.assertEqual(self.m.id_, t.id_)
-        self.assertEqual(self.m.Irms, t.Irms)
+        self.assertEqual(self.mac*10+self.m.id_, t.id_)
+        self.assertAlmostEqual(self.m.Irms, t.Irms, 5)
 
 
 class TransportTest(unittest.TestCase):
@@ -54,7 +54,7 @@ class TransportTest(unittest.TestCase):
         self.assertEqual('j', self.t.read())
 
     def test_read_full_mcu_pkg(self):
-        self.t._ans("\x7E\xAA\x55\x00\x00\x7F")
+        self.t._ans("\x7E\xAA\x55\x7F")
         pkg = self.t.read_package_from_xmega()
         self.assertEqual((0xAA, 0x55), tuple(pkg))
 
@@ -65,24 +65,24 @@ class TransportTest(unittest.TestCase):
         self.assertIsNone(pkg)
 
     def test_stuffed_full_mcu_pkg(self):
-        self.t._ans("\x7E\x7D\x8A\x45\x00\x00\x7F")
+        self.t._ans("\x7E\x7D\x8A\x45\x7F")
         pkg = self.t.read_package_from_xmega()
         self.assertEqual((0xAA, 0x45), tuple(pkg))
 
     def test_encode_no_stuff(self):
         a = self.t.encode_for_xmega("\x41\x42")
-        self.assertEquals("".join(a), "\x7E\x41\x42\x00\x00\x7F")
+        self.assertEquals("".join(a), "\x7E\x41\x42\x7F")
 
     def test_encode_with_stuff(self):
         a = self.t.encode_for_xmega("\x41\x7F\x42")
-        self.assertEquals("".join(a), "\x7E\x41\x7D\x5F\x42\x00\x00\x7F")
+        self.assertEquals("".join(a), "\x7E\x41\x7D\x5F\x42\x7F")
 
     def test_autogen(self):
         c = MCUComm()
         auto_t = TestTransport(True)
         pkg = auto_t.read_package_from_xmega()
         m = c.read(pkg)
-        self.assertEqual(m.id_, 42)
+        self.assertTrue(m.id_ in [1, 2, 3, 4, 5, 6])
 
 
 if __name__ == '__main__':
